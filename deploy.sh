5464fdec1711a86eaffbdec8d705c76602e83ba4
andrew.rapp <andrew.rapp@42c8444e-1c8e-11de-a108-cd2f117ce590>
2009-03-31 02:20:49 +0000

    - added Ant script - removed obsolete files
    git-svn-id: https://xbee-arduino.googlecode.com/svn/trunk@5 42c8444e-1c8e-11de-a108-cd2f117ce590

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp XBee.* .././../../devsoft/arduino-0014/hardware/libraries/XBee/
-rm .././../../devsoft/arduino-0014/hardware/libraries/XBee/XBee.o
-
-
--- dist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# TODO exclude .svn and applet dirs
-mkdir -p XBee
-
-cp *.cpp *.h XBee
-cp -r sketches XBee
